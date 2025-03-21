8ef05f7a3375ac1c8f30f5fc181fa7d45d35b744
amscu-dev <amarinescu.dev@gmail.com>
2025-03-21 11:05:39 +0200

    3: configure tailwind + prettier-tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>hello vite</div>;
+  return (
+    <div className="flex h-full items-center justify-center bg-slate-500">
+      <p className="rounded-md bg-slate-100 px-20 py-8 text-6xl font-extralight text-violet-500">
+        hello vite
+      </p>
+    </div>
+  );
 }
 
 export default App;
